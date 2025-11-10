1be8354594f60f4ccfe7531ecf07a5f2ee4debfa
Eraser <113839336+Eraser-It-Re@users.noreply.github.com>
2025-11-10 11:36:47 +0900

    伊藤 - GoshuinBook,Ownedgoshuin,RegdGoshuinのBean作成

--- src/bean/OwnedGoshuin.java
+++ src/bean/OwnedGoshuin.java
@@ -4,4 +4,8 @@ import java.io.Serializable;
 
 public class OwnedGoshuin implements Serializable {
 
+	private RegdGoshuin goshuin;
+	private int userId;
+	private int goshuinBookId;
+
 }
--- src/bean/RegdGoshuin.java
+++ src/bean/RegdGoshuin.java
@@ -1,7 +1,15 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class RegdGoshuin implements Serializable {
 
+	private int id;
+	private ShrineAndTemple shrineAndTemple;
+	private String saleStartDate;
+	private String saleEndDate;
+	private String imagePath;
+	private LocalDateTime updatedAt;
+	private LocalDateTime createdAt;
 }
