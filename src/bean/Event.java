3e3dd4af7eb11caa637f7bbd1dd3f5deaec4a4a1
Mori-H <ymg2465058@stu.o-hara.ac.jp>
2025-11-10 12:19:02 +0900

    hennsyuuzumi

--- src/bean/Event.java
+++ src/bean/Event.java
@@ -1,7 +1,70 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Event implements Serializable {
+
+	private int id;
+
+	private String Title;
+
+	private String Text;
+
+	private String imagePath;
+
+	private LocalDateTime updateAt;
+
+	private LocalDateTime createdAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getText() {
+		return Text;
+	}
+
+	public void setText(String text) {
+		Text = text;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
 
 }
