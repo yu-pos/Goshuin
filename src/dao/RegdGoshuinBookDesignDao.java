e0f9df1f85cfaf2d9909d56f6b1d7c54885f4e14
Unko44 <ymg2465086@stu.o-hara.ac.jp>
2025-11-10 14:49:42 +0900

    daoの処理をコメントで書きました

--- src/dao/RegdGoshuinBookDesignDao.java
+++ src/dao/RegdGoshuinBookDesignDao.java
@@ -2,4 +2,7 @@ package dao;
 
 public class RegdGoshuinBookDesignDao {
 
+	//登録済み御朱印帳デザイン一覧を取得():list<regdGoshuinBookDesign>
+
+
 }
--- src/dao/RegdGoshuinDao.java
+++ src/dao/RegdGoshuinDao.java
@@ -1,10 +1,10 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class RegdGoshuinDao extends Dao {
+
+	//読み取ったQRコードに記載されている神社仏閣IDから御朱印を検索():regdGoshuin
+
+	//御朱印情報を登録():boolean
+
 
 }
