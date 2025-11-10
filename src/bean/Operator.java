f7bf9509f24f1c814c3a84c9ad0356eb1ba6ce18
oishi-0805 <ymg2465080@stu.o-hara.ac.jp>
2025-11-10 12:28:41 +0900

    大石　deen作成

--- src/bean/Operator.java
+++ src/bean/Operator.java
@@ -1,7 +1,52 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Operator implements Serializable {
+
+	private int id;
+	private  String password;
+	private  boolean isAdmin;
+	private boolean firstLoginCompleted;
+	private LocalDateTime updatedAt;
+	private LocalDateTime createdAt;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public boolean isFirstLoginCompleted() {
+		return firstLoginCompleted;
+	}
+	public void setFirstLoginCompleted(boolean firstLoginCompleted) {
+		this.firstLoginCompleted = firstLoginCompleted;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
 
 }
--- src/bean/OwnedGoshuinBookDesignGroup.java
+++ src/bean/OwnedGoshuinBookDesignGroup.java
@@ -1,7 +1,36 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class OwnedGoshuinBookDesignGroup implements Serializable {
 
+	private RegdGoshuinBookDesignGroup goshuinBookDesignGroup;
+	private  int userId;
+	private LocalDateTime updatedAt;
+	private LocalDateTime createdAt;
+	public RegdGoshuinBookDesignGroup getGoshuinBookDesignGroup() {
+		return goshuinBookDesignGroup;
+	}
+	public void setGoshuinBookDesignGroup(RegdGoshuinBookDesignGroup goshuinBookDesignGroup) {
+		this.goshuinBookDesignGroup = goshuinBookDesignGroup;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 }
