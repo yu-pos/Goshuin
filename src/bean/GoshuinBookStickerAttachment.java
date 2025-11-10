e4a17b07180166abce207632de05b247e8445a2f
yu-pon <ymg2465079@stu.o-hara.ac.jp>
2025-11-10 12:05:39 +0900

    bean作成

--- src/bean/GoshuinBookStickerAttachment.java
+++ src/bean/GoshuinBookStickerAttachment.java
@@ -1,5 +1,77 @@
 package bean;
 
+import java.time.LocalDateTime;
+
 public class GoshuinBookStickerAttachment {
+
+	private int goshuinBookId;
+
+	private RegdGoshuinBookSticker goshuinBookSticker;
+
+	private double xPos;
+
+	private double yPos;
+
+	private double rotation;
+
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime createdAt;
+
+	public int getGoshuinBookId() {
+		return goshuinBookId;
+	}
+
+	public void setGoshuinBookId(int goshuinBookId) {
+		this.goshuinBookId = goshuinBookId;
+	}
+
+	public RegdGoshuinBookSticker getGoshuinBookSticker() {
+		return goshuinBookSticker;
+	}
+
+	public void setGoshuinBookSticker(RegdGoshuinBookSticker goshuinBookSticker) {
+		this.goshuinBookSticker = goshuinBookSticker;
+	}
+
+	public double getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+
+	public double getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
+	}
+
+	public double getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(double rotation) {
+		this.rotation = rotation;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
--- src/bean/OwnedGoshuinBookSticker.java
+++ src/bean/OwnedGoshuinBookSticker.java
@@ -1,7 +1,48 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class OwnedGoshuinBookSticker implements Serializable {
+
+	private RegdGoshuinBookSticker goshuinBookSticker;
+
+	private int userId;
+
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime createdAt;
+
+	public RegdGoshuinBookSticker getGoshuinBookSticker() {
+		return goshuinBookSticker;
+	}
+
+	public void setGoshuinBookSticker(RegdGoshuinBookSticker goshuinBookSticker) {
+		this.goshuinBookSticker = goshuinBookSticker;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
