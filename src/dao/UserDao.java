ee1c9a10420aefb54c8ac6b94a387e9764dd782c
Eraser <113839336+Eraser-It-Re@users.noreply.github.com>
2025-11-11 09:41:25 +0900

    伊藤 - UserDao修正

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -13,8 +13,8 @@ public class UserDao extends Dao {
 	 * 処理一覧
 	 * ・利用者情報を更新(User:user) : boolean - Save(User:user) - 新規登録・使用中御朱印帳変更・御朱印カウント・ランクの更新をこれで行う
 	 * ・IDで利用者情報を取得(id:int) : User - SearchById(id:int)
-	 * ・ポイント付与(point:int) : boolean - AddPoint(point:int)
-	 * ・ポイント消費(point:int) : boolean - SubPoint(point:int)
+	 * ・ポイント付与(User:user, point:int) : boolean - AddPoint(User:user, point:int)
+	 * ・ポイント消費(User:user, point:int) : boolean - SubPoint(User:user, point:int)
 	 * ・ログインする(telNumber:String, password:String) : boolean - login(telNumber:String, password:String)
 	 */
 
