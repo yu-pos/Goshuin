1ef366fe5a96275db4c3786a1e4c70a925ebc283
kuroda-yuki2005 <ymg2465070@stu.o-hara.ac.jp>
2025-11-11 09:59:20 +0900

    ｑ

--- src/bean/RegdGoshuinBookSticker.java
+++ src/bean/RegdGoshuinBookSticker.java
@@ -1,7 +1,60 @@
 package bean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class RegdGoshuinBookSticker implements Serializable {
 
+	private int id;
+	
+	private String name;
+	
+	private String imagePath;
+	
+	private LocalDateTime  updatedAt;
+
+	private  LocalDateTime createdAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
 }
+	
