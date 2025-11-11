13819cd5c09fc7f804b0360528511df651110581
yu-pon <ymg2465079@stu.o-hara.ac.jp>
2025-11-11 10:01:05 +0900

    Daoメッセージ追加

--- src/dao/OwnedGoshuinBookStickerDao.java
+++ src/dao/OwnedGoshuinBookStickerDao.java
@@ -1,5 +1,12 @@
 package dao;
 
-public class OwnedGoshuinBookStickerDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OwnedGoshuinBookStickerDao extends Dao {
+
+	//所持御朱印帳ステッカー情報を登録():boolean
 
 }
