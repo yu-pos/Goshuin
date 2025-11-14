8d8d8a69e56f3940a9590a4cfd65577b0fac8d67
kuroda-yuki2005 <ymg2465070@stu.o-hara.ac.jp>
2025-11-14 10:19:11 +0900

    ReviewDao完了

--- src/dao/ReviewDao.java
+++ src/dao/ReviewDao.java
@@ -2,7 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import bean.Review;
 
@@ -40,7 +43,7 @@ public class ReviewDao extends Dao {
 	            count = statement.executeUpdate();
 
 	            // 1件以上登録できれば true
-	            
+
 	        } catch (Exception e) {
 	            throw e; // 呼び出し元に例外を投げる
 	        } finally {
@@ -62,7 +65,7 @@ public class ReviewDao extends Dao {
 	            }
 	        }
 
-	 
+
 
 		if (count == 1) {
 			// 実行件数1件の場合
@@ -72,7 +75,126 @@ public class ReviewDao extends Dao {
 			return false;
 		}
 	 }
+
+
+
+
+
+public List<Review> searchById(int shrine_and_temple_id) throws Exception{
+	// 商品券リストを初期化
+	List<Review> list = new ArrayList<>();
+	// コネクションを確立
+	Connection connection = getConnection();
+	// プリペアードステートメント
+	PreparedStatement statement = null;
+
+	try {
+		// プリペアードステートメントにSQL文をセット
+		statement = connection.prepareStatement(
+	            "SELECT  id,shrine_and_temple_id,user_id,text,image_path"
+	            +"FROM review WHERE shurain_and_temple_id = ?"
+		);
+
+		// 神社仏閣IDをバインド
+		statement.setInt(1, shrine_and_temple_id);
+
+		// SQLを実行
+		ResultSet resultSet = statement.executeQuery();
+
+		// 複数件をリストに追加
+        while (resultSet.next()) {
+            Review review = new Review();
+            review.setId(resultSet.getInt("id"));
+            review.setUserId(resultSet.getInt("user_id"));
+            review.setShrineAndTempleId(resultSet.getInt("shrine_and_temple_id"));
+            review.setImagePath(resultSet.getString("image_path"));
+            review.setText(resultSet.getString("text"));
+
+            list.add(review);
+
+        }
+
+	} catch (Exception e) {
+		throw e;
+	} finally {
+		// ステートメントを閉じる
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException sqle) {
+                throw sqle;
+            }
+        }
+        // コネクションを閉じる
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException sqle) {
+                throw sqle;
+            }
+        }
+
+	}
+
+	return list; // 空リストか商品券一覧
 }
 
 
-	 
+
+public boolean update(Review review) throws Exception {
+	// コネクションを確立
+	Connection connection = getConnection();
+	// プリペアードステートメント
+	PreparedStatement statement = null;
+
+	// 実行件数
+	int count = 0;
+
+	try {
+
+		// 利用者が存在した場合、情報を更新
+		// プリペアードステートメントにUPDATE文をセット
+		statement = connection.prepareStatement("UPDATE review SET shrine_and_temple_id = ?,  id = ?, text= ?, "
+				+ " = ?, image_path = ?,   updated_at = CURRENT_DATETIME WHERE id = ?");
+		// プリペアードステートメントに値をバインド
+		statement.setInt(1, review.getShrineAndTempleId());
+		statement.setInt(2, review.getId());
+		statement.setString(3, review.getText());
+		statement.setString(5, review.getImagePath());
+
+
+
+
+		// プリペアードステートメントを実行
+		count = statement.executeUpdate();
+
+	} catch (Exception e) {
+		throw e;
+	} finally {
+		// プリペアードステートメントを閉じる
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+		// コネクションを閉じる
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+	}
+
+	if (count == 1) {
+		// 実行件数1件の場合
+		return true;
+	} else {
+		// 実行件数がそれ以外の場合
+		return false;
+	}
+}
+}
