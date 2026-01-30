47e78ef6e3c6f8ae47c400257c2b81861b388075
Eraser <113839336+Eraser-It-Re@users.noreply.github.com>
2026-01-30 10:52:35 +0900

    伊藤 - FrontController修正

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -15,6 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 @MultipartConfig
 public class FrontController extends HttpServlet {
 
+	//実行中環境がローカルかEC2上か判定
+	private static boolean isProd() {
+	    String env = System.getenv("APP_ENV");
+	    return "prod".equalsIgnoreCase(env);
+	}
+
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		try {
@@ -30,10 +37,14 @@ public class FrontController extends HttpServlet {
 
 
 		} catch (Exception e) {
-			StringWriter sw = new StringWriter();
-	        PrintWriter pw = new PrintWriter(sw);
-	        e.printStackTrace(pw); // スタックトレースをPrintWriterに書き込む
-	        req.setAttribute("e", sw.toString()); ; // 文字列に変換 [1]
+			if (isProd()) {
+				StringWriter sw = new StringWriter();
+		        PrintWriter pw = new PrintWriter(sw);
+		        e.printStackTrace(pw); // スタックトレースをPrintWriterに書き込む
+		        req.setAttribute("e", sw.toString()); ; // 文字列に変換 [1]
+			} else {
+				e.printStackTrace();
+			}
 			// エラーページへリダイレクト
 			req.getRequestDispatcher("error.jsp").forward(req, res);
 		}
